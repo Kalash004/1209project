6542fae344a3c136abfa72a3788a97ae5df1f673
Anton <kalashnikov004@gmail.com>
2022-09-16 20:06:12 +0200

    added parts of 2nd task

---
+++ Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Line
+    {
+        private Point point1;
+        private Point point2;
+
+        public Line(Point point1, Point point2)
+        {
+
+        }
+
+        public Line()
+        {
+
+        }
+
+        public Point Point
+        {
+            get { return point1; }
+            set { point1 = value; }
+        }
+    }
+}
