878ba254f9f194fb1aa0e036e72ae9fcc278fe1b
Anton <kalashnikov004@gmail.com>
2022-09-16 19:19:31 +0200

    added project files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using _1209project;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Interval inter = new Interval(40, 45);
+        Console.WriteLine(inter.ToString());
+        Interval inter2 = new Interval(60, 70);
+        Console.WriteLine(inter2.ToString()); ;
+        Interval inter3 = inter.findUnion(inter2);
+        Console.WriteLine(value: inter3.ToString());
+    }
+
+}
