b1b84b2243b782d842eef4b913c5570d0008c480
Anton <kalashnikov004@gmail.com>
2022-09-17 16:37:48 +0200

    type finder done, checking task 3 build

--- Triangle.cs
+++ Triangle.cs
@@ -32,7 +32,7 @@ namespace ConsoleApp1
 
         public Triangle(Point a, Point b, Point c)
         {
-            if (a.pointEquals(b) && a.pointEquals(c) && b.pointEquals(c)) throw new ArgumentException("Points cant have same coordinates"); 
+            if (a.pointEquals(b) && a.pointEquals(c) && b.pointEquals(c)) throw new ArgumentException("Points cant have same coordinates");
             if (a.X == b.X && b.X == c.X || a.Y == b.Y && b.Y == c.Y)
             {
                 throw new ArgumentException("This triangle is impossible because it clips inside it self");
@@ -71,11 +71,43 @@ namespace ConsoleApp1
 
         public Triangle_Type findType()
         {
+            //Equilateral, // 3 equal sides
+            //Isosceles, // 2 equal sides
+            //Scalene // no equal sides
+            double la = lineA.getLength();
+            double lb = lineB.getLength();
+            double lc = lineC.getLength();
+
+
             Triangle_Type type = Triangle_Type.Scalene;
-
-
-
+            if (la == lb && lb == lc)
+            {
+                type = Triangle_Type.Equilateral;
+            }
+            else if (la == lb && lb != lc || la == lc && lc != lb || lb == lc && lc != la)
+            {
+                type = Triangle_Type.Isosceles;
+            }
             return type;
+        }
+
+        public Triangle_Type_Angles fingAngleType()
+        {
+            //Right_angled, // one angle is 90 deg
+            //Acute_angled, // all angles < 90 deg
+            //Obtuse_angled // one angle is > 90 deg
+
+            Triangle_Type_Angles type = Triangle_Type_Angles.Acute_angled;
+            double[] angles = findAngles();
+            bool nintyplus = false;
+            for (int i = 0; i < angles.Length; i++)
+            {
+                double current = angles[i];
+                if (current > 90) return type = Triangle_Type_Angles.Obtuse_angled;
+                if (current == 90) return type = Triangle_Type_Angles.Right_angled;     
+            }
+            return type;
+
         }
 
         public double[] findAngles()
@@ -98,15 +130,15 @@ namespace ConsoleApp1
 
 
             angles[0] = radToDeg(A);
-            angles [1] = radToDeg(B);
-            angles [2] = radToDeg(C);
+            angles[1] = radToDeg(B);
+            angles[2] = radToDeg(C);
 
             return angles;
         }
 
         public double radToDeg(double radians)
         {
-            double degrees =  radians * (180 / Math.PI);
+            double degrees = radians * (180 / Math.PI);
             return (degrees);
         }
     }
