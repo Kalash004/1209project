8bc7f22929364ae30e2bdb134816e814ecbaa3de
Anton <kalashnikov004@gmail.com>
2022-09-17 17:44:00 +0200

    Working on food

--- Food.cs
+++ Food.cs
@@ -10,11 +10,49 @@ namespace ConsoleApp1
     {
         private String name;
         private String description;
-        private int mass;
-        private double kcal;
-        public String Name { get { return name; } set { name = value; } }
-        public String Description { get { return description; } set { description = value; } }
-        public int Mass { get { return mass; } set { mass = value; } }
-        public double Kcal { get { return kcal; } set { kcal = value; } }
+        private double mass;
+        private double? kcal;
+        private double? kj;
+        
+        public Food(String name, String description, double mass, double? kcal, double? kj)
+        {
+            this.name = name;
+            this.description = description; 
+            this.mass = mass; 
+            this.kcal = kcal;
+            this.kj = kj;  
+        }
+
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public double Mass
+        {
+            get { return mass; }
+            set
+            {
+                if (value < 0) throw new Exception("Mass cant be 0");
+                mass = value;
+            }
+        }
+        public double Kcal
+        {
+            get { return kcal; }
+            set 
+            {
+                if (value < 0) throw new Exception("kcal cant be 0");
+                kcal = value;
+            }
+        }
+
+        
     }
 }
