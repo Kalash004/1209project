6dd2181ce2562057ef7de26f7b3ea4a68b7b891e
Anton <kalashnikov004@gmail.com>
2022-09-17 10:48:33 +0200

    Started working on Triangle

---
+++ Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Triangle
+    {
+        private Point a;
+        private Point b;
+        private Point c;
+        private Line lineA;
+        private Line lineB;
+        private Line lineC;
+
+
+
+    }
+}
